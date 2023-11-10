fa5248ea82c4698fff39cf3845b28ec17c5707f5
R1D3R175 <r1.d3.r175@gmail.com>
2023-11-10 09:26:23 +0100

    feat: initialize ORM components

---
+++ data/db/__init__.py
@@ -0,0 +1,9 @@
+"""
+    Initialize the database engine + base model
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+
+engine = create_engine("sqlite://db.sqlite3")
+class Base(DeclarativeBase):
+    pass
