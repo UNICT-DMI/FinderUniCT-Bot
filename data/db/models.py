cc42bc2ef316728fa5e0a40969fce2ee883a99f1
R1D3R175 <r1.d3.r175@gmail.com>
2023-11-10 09:26:45 +0100

    feat: define `User` ORM class

---
+++ data/db/models.py
@@ -0,0 +1,21 @@
+"""
+    Definition of database tables
+"""
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from . import Base
+
+class User(Base):
+    """
+        User table, maps the following fields:
+        - id (int): primary key, autoincrement
+        - email (str): hexdigest of salted user's email hashed with sha256
+        - salt (str): random string used to salt the user's email (8 bytes)
+        - chat_id (int): id of the chat the user is in
+    """
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(80))
+    salt: Mapped[str] = mapped_column(String(16))
+    chat_id: Mapped[int]
