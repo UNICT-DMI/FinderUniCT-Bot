4f7d84e412dde220d4debd9f78b9d3e35f4cece1
R1D3R175 <r1.d3.r175@gmail.com>
2023-11-11 20:24:43 +0100

    fix: add code to create table fix: use absolute path to address database position

--- data/db/__init__.py
+++ data/db/__init__.py
@@ -2,9 +2,17 @@
     Initialize the database engine + base model
 """
 # pylint: disable=too-few-public-methods
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 
-engine = create_engine("sqlite://db.sqlite3")
+db_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "db.sqlite3")
+engine = create_engine("sqlite:///" + db_path)
 class Base(DeclarativeBase):
     pass
+
+# pylint: disable=wrong-import-position
+from .models import User
+
+Base.metadata.create_all(engine)
